67b2e5cc60f51277f6979e2c7dd76e8c59d8df31
Binod kumar <binod0506devops@gmail.com>
2026-06-14 16:36:33 +0530

    Initial project setup with Jenkinsfile and code

---
+++ New folder/src/main/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public String getMessage() {
+        return "Hello, Jenkins!";
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        System.out.println(app.getMessage());
+    }
+}
---
+++ New folder/src/test/AppTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testGetMessage() {
+        App app = new App();
+        String message = app.getMessage();
+        assertEquals("Hello, Jenkins!", message);
+    }
+}
